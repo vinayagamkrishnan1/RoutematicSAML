8bf509866280636da070e855aab5ca2f0061011e
vinayagam <vinayagamkrishnan1@gmail.com>
2024-01-11 10:44:38 +0530

    Correction

--- android/app/src/main/java/com/routematicsamlapp/MainApplication.kt
+++ android/app/src/main/java/com/routematicsamlapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.routematicsmal
+package com.routematicsamlapp
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/RoutematicSAMLappTests/RoutematicSAMLappTests.m
+++ ios/RoutematicSAMLappTests/RoutematicSAMLappTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RoutematicSMALTests : XCTestCase
+@interface RoutematicSAMLappTests : XCTestCase
 
 @end
 
-@implementation RoutematicSMALTests
+@implementation RoutematicSAMLappTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
